652235c0c1a1eeeb759e866263bc7a96d27c8af9
Nicolas Picon <nicolas.picon@in-tact.fr>
2021-03-08 18:21:47 +0100

    Add user (stateful)

--- exo1/lib/screens/users/add_user_toolbar.dart
+++ exo1/lib/screens/users/add_user_toolbar.dart
@@ -1,20 +1,58 @@
+import 'package:exo1/domain/user.dart';
 import 'package:flutter/material.dart';
 
-class AddUserToolbar extends StatelessWidget {
+class AddUserToolbar extends StatefulWidget {
+  AddUserToolbar({required this.onUserSubmitted});
+
+  final ValueSetter<User> onUserSubmitted;
+
+  @override
+  _AddUserToolbarState createState() => _AddUserToolbarState();
+}
+
+class _AddUserToolbarState extends State<AddUserToolbar> {
+  final controller = TextEditingController();
+  VoidCallback? addAction;
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8.0),
       child: Row(
         children: [
-          Flexible(child: TextField()),
+          Flexible(
+            child: TextField(
+              controller: controller,
+              onChanged: (textValue) {
+                setState(() {
+                  if (textValue.isEmpty) {
+                    addAction = null;
+                  } else {
+                    addAction = submitUser;
+                  }
+                });
+              },
+            ),
+          ),
           ElevatedButton.icon(
-            onPressed: null,
+            onPressed: addAction,
             icon: Icon(Icons.person_add),
             label: Text("Add person"),
           )
         ],
       ),
     );
+  }
+
+  void submitUser() {
+    var user = User(name: controller.text);
+    controller.text = "";
+    widget.onUserSubmitted(user);
   }
 }
--- exo1/lib/screens/users/users_screen.dart
+++ exo1/lib/screens/users/users_screen.dart
@@ -1,16 +1,28 @@
+import 'package:exo1/domain/user.dart';
 import 'package:exo1/domain/user_repository.dart';
 import 'package:exo1/screens/users/user_list_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'add_user_toolbar.dart';
 
-class UsersScreen extends StatelessWidget {
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
   final _repository = UserRepository.get();
+
+  late List<User> users;
+
+  @override
+  void initState() {
+    super.initState();
+    users = _repository.getUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final users = _repository.getUsers();
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Users"),
@@ -20,9 +32,16 @@ class UsersScreen extends StatelessWidget {
           Flexible(
             child: UserListWidget(users: users),
           ),
-          AddUserToolbar(),
+          AddUserToolbar(onUserSubmitted: addUser),
         ],
       ),
     );
+  }
+
+  void addUser(User user) {
+    _repository.addUser(user);
+    setState(() {
+      users = _repository.getUsers();
+    });
   }
 }
