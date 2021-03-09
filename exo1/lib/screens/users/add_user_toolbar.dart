54d618a0e7d29876a5b406e572ca84f5ca0ac5d0
Nicolas Picon <nicolas.picon@in-tact.fr>
2021-03-09 20:00:00 +0100

    Add user on submit

--- exo1/lib/screens/users/add_user_toolbar.dart
+++ exo1/lib/screens/users/add_user_toolbar.dart
@@ -38,6 +38,7 @@ class _AddUserToolbarState extends State<AddUserToolbar> {
                   }
                 });
               },
+              onSubmitted: (_) => addAction?.call(),
             ),
           ),
           ElevatedButton.icon(
