bd368c954f5107493f7bec11c31a2fd70bbed6b8
Nicolas Picon <nicolas.picon@in-tact.fr>
2021-03-16 09:38:49 +0100

    Agency Details layout

--- exo2/lib/screens/details/agency_details_screen.dart
+++ exo2/lib/screens/details/agency_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:exo2/domain/agency.dart';
+import 'package:exo2/utils/lorem_fetcher.dart';
 import 'package:flutter/material.dart';
 
 class AgencyDetailsScreen extends StatelessWidget {
@@ -8,10 +9,51 @@ class AgencyDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text(agency.city),
+    var textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(title: Text("Agency details")),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(agency.company.logo, width: 56),
+                  SizedBox(width: 8),
+                  Text(
+                    agency.city,
+                    style: textTheme.headline4,
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              FutureBuilder<List<String>>(
+                future: fetchLorem(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: snapshot.data!
+                          .map(
+                            (paragraph) => Padding(
+                              padding: EdgeInsets.only(bottom: 8),
+                              child: Text(
+                                paragraph,
+                                style:
+                                    textTheme.bodyText2!.copyWith(fontSize: 16),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
