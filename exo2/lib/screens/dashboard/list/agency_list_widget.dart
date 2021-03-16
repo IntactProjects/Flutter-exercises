7dff027c5cccce176fd20dba3d22477a6f2e2396
Nicolas Picon <nicolas.picon@in-tact.fr>
2021-03-16 09:29:51 +0100

    Agency List layout

---
+++ exo2/lib/screens/dashboard/list/agency_list_widget.dart
@@ -0,0 +1,40 @@
+import 'package:exo2/domain/agency.dart';
+import 'package:flutter/material.dart';
+
+class AgencyListWidget extends StatelessWidget {
+  final List<Agency> agencies;
+  final ValueSetter<Agency>? onAgencyClicked;
+
+  const AgencyListWidget({required this.agencies, this.onAgencyClicked});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: agencies.length,
+      itemBuilder: (context, index) => AgencyTile(
+        agency: agencies[index],
+        onAgencyClicked: onAgencyClicked,
+      ),
+    );
+  }
+}
+
+class AgencyTile extends StatelessWidget {
+  final Agency agency;
+  final ValueSetter<Agency>? onAgencyClicked;
+
+  const AgencyTile({required this.agency, this.onAgencyClicked});
+
+  @override
+  Widget build(BuildContext context) => Hero(
+        tag: getHeroTag(agency, 'title'),
+        child: Material(
+          child: ListTile(
+            leading: Image.asset(agency.company.logo),
+            title: Text(agency.city),
+            subtitle: Text(agency.company.label),
+            onTap: () => onAgencyClicked?.call(agency),
+          ),
+        ),
+      );
+}
--- exo2/lib/screens/dashboard/list/list_screen.dart
+++ exo2/lib/screens/dashboard/list/list_screen.dart
@@ -1,15 +1,27 @@
 import 'package:exo2/domain/agency.dart';
 import 'package:flutter/material.dart';
 
+import 'agency_list_widget.dart';
+
 class ListScreen extends StatelessWidget {
   final List<Agency>? agencies;
   final ValueSetter<Agency>? onAgencyClicked;
 
-  const ListScreen({Key? key, required this.agencies, this.onAgencyClicked})
-      : super(key: key);
+  const ListScreen({
+    Key? key,
+    required this.agencies,
+    this.onAgencyClicked,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    if (agencies != null) {
+      return AgencyListWidget(
+        agencies: agencies!,
+        onAgencyClicked: onAgencyClicked,
+      );
+    } else {
+      return Center(child: CircularProgressIndicator.adaptive());
+    }
   }
 }
