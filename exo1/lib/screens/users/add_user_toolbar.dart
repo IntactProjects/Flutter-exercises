b161f81ed36a34b5fedfa2a785aae8322a98b4c5
Christophe-R <ch.reubrecht@gmail.com>
2021-03-24 12:52:53 +0100

    exo1

--- exo1/lib/screens/users/add_user_toolbar.dart
+++ exo1/lib/screens/users/add_user_toolbar.dart
@@ -1,15 +1,40 @@
+import 'package:exo1/domain/user.dart';
+import 'package:exo1/domain/user_repository.dart';
 import 'package:flutter/material.dart';
 
-class AddUserToolbar extends StatelessWidget {
+class AddUserToolbar extends StatefulWidget {
+  @override
+  _AddUserToolbarState createState() => _AddUserToolbarState();
+}
+
+class _AddUserToolbarState extends State<AddUserToolbar> {
+  var newUser = '';
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8.0),
       child: Row(
         children: [
-          Flexible(child: TextField()),
+          Flexible(
+              child: TextField(
+            controller: _controller,
+            onChanged: (String value) {
+              setState(() {
+                newUser = value;
+              });
+            },
+          )),
           ElevatedButton.icon(
-            onPressed: null,
+            onPressed: newUser.length != 0
+                ? () {
+                    UserRepository.get().addUser(new User(name: newUser));
+                    _controller.clear();
+                    setState(() {
+                      newUser = '';
+                    });
+                  }
+                : null,
             icon: Icon(Icons.person_add),
             label: Text("Add person"),
           )
--- exo1/lib/screens/users/users_screen.dart
+++ exo1/lib/screens/users/users_screen.dart
@@ -1,15 +1,27 @@
+import 'package:exo1/domain/user.dart';
 import 'package:exo1/domain/user_repository.dart';
 import 'package:exo1/screens/users/user_list_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'add_user_toolbar.dart';
 
-class UsersScreen extends StatelessWidget {
+class UsersScreen extends StatefulWidget {
+  @override
+  _UsersScreenState createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
   final _repository = UserRepository.get();
 
   @override
   Widget build(BuildContext context) {
-    final users = _repository.getUsers();
+    var users = _repository.getUsers();
+
+    _repository.observeUsers().listen((event) {
+      setState(() {
+          users = event;
+      });
+    });
 
     return Scaffold(
       appBar: AppBar(
