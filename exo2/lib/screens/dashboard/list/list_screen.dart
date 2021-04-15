130768034ffa75b96786c2c3cb26d4a031e6e8b4
Félix Boquet <felix.boquet@in-tact.fr>
2021-04-15 17:03:57 +0200

    Adding Google maps screen

--- exo2/lib/screens/dashboard/list/list_screen.dart
+++ exo2/lib/screens/dashboard/list/list_screen.dart
@@ -4,10 +4,8 @@ import 'agency_list_item.dart';
 
 class ListScreen extends StatelessWidget {
   final List<Agency>? agencies;
-  final ValueSetter<Agency>? onAgencyClicked;
 
-  const ListScreen({Key? key, required this.agencies, this.onAgencyClicked})
-      : super(key: key);
+  const ListScreen({Key? key, required this.agencies}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
